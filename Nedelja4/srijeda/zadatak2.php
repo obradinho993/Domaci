1fd82c107b6cb20513b6656ed62aa02adf5e51f7
Obrad Babic <polo26282@gmail.com>
2021-05-11 15:33:25 +0200

    drugi pon

---
+++ Nedelja4/srijeda/zadatak2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 3;
+        $b = 5;
+        $c = 8;
+        $d = $a + $b + $c ;
+
+        echo 'Zbir brojeva a, b i c je';
+        echo $d ;
+    ?>
+</body>
+</html>
