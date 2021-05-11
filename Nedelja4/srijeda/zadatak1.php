1093283df928d1f96763108e5e2407d2297b6953
Obrad Babic <polo26282@gmail.com>
2021-05-11 15:25:26 +0200

    prvi zadatak pon

---
+++ Nedelja4/srijeda/zadatak1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x = 200 ;
+        $y = 80 ;
+
+        $proizvod = $x * $y;
+        $kolicnik = $x / $y;
+        $zbir = $x + $y;
+        $razlika = $x - $y;
+
+        echo "<p> $proizvod </p>";
+        echo  "<p> $kolicnik </p>";
+        echo "<p> $zbir</p>" ;
+        echo "<p>$razlika</p>" ;
+  
+    ?>
+</body>
+</html>
