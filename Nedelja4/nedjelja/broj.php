652968687ce9fcd76c3b4855299cab05e48131c0
Obrad Babic <polo26282@gmail.com>
2021-05-16 16:37:23 +0200

    cetvrtak1

--- Nedelja4/nedjelja/broj.php
+++ Nedelja4/nedjelja/broj.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .green{
+        color:green;
+    }
+    .red{
+        color:red;
+    }
+    
+    </style>
 </head>
 <body>
     <?php
@@ -19,6 +28,17 @@
         echo "<p style='color:green'>$b<p> <br>
         <p style='color:red'>$a<p>";
     }
+
+   
+    if($a>$b){
+        echo "<p class = 'green'>$a<p> <br>
+              <p class = 'red'>$b<p>";
+    }
+    else{
+        echo "<p class = 'green'>$b<p> <br>
+              <p class = 'red'>$a<p>";
+    }
+
 
     
     ?>
